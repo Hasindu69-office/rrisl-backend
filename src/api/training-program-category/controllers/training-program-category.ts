973bc067484add58464d98a15f97d5912b9b77e0
Hasindu69-office <hasindu@graphics.lk>
2026-06-10 08:35:22 +0530

    Implemented the avisory services section in the backend

---
+++ src/api/training-program-category/controllers/training-program-category.ts
@@ -0,0 +1,7 @@
+/**
+ * training-program-category controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::training-program-category.training-program-category');
