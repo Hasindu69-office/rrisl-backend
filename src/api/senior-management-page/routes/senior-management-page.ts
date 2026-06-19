9e293fcd9207408fe0a7f6bfde03761ff248058e
Hasindu69-office <hasindu@graphics.lk>
2026-06-19 08:27:08 +0530

    Implemented senior management to backend

---
+++ src/api/senior-management-page/routes/senior-management-page.ts
@@ -0,0 +1,7 @@
+/**
+ * senior-management-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::senior-management-page.senior-management-page');
